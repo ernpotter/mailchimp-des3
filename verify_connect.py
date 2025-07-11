624b85328203c2f2952a85cae446868b08b9836c
ErinPotter <erinpotte@gmail.com>
2025-07-11 11:20:00 +0100

    Adding better documentation to verify_connect.py

--- verify_connect.py
+++ verify_connect.py
@@ -1,22 +1,26 @@
+# Verifying connection to MailChimp's API
+# https://github.com/mailchimp/mailchimp-marketing-python
+
+# Importing required libraies 
 from dotenv import load_dotenv
 import os
 import mailchimp_marketing as MailchimpMarketing
-from mailchimp_marketing import Client
-from mailchimp_marketing.api_client import ApiClientError
-import json
-from datetime import date, timedelta
 
-# Load the env variables
+# Load the env variables so we can grab the API keys
 load_dotenv()
+
+# Read the .env file and load the key and server prefix from
 api_key = os.getenv('MAILCHIMP_API_KEY')
 server_prefix = os.getenv('MAILCHIMP_SERVER_PREFIX')
 
-client = MailchimpMarketing.Client() #Initialize the client
+# Initialize the client. Creates a new instance of the mailchimp client so we can make API calls 
+client = MailchimpMarketing.Client()
+
+# Configure the client with the API key and server prefix
 client.set_config({
     "api_key": api_key,
-    "server": server_prefix  # Configure client with API key
+    "server": server_prefix
 })
-
-
+# Pinging the API to verify connection. Hey Mailchimp, can you hear me?
 response = client.ping.get()
 print(response)
