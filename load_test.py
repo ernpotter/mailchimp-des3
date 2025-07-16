5f940c067ccfcdd036151cc427190b0b9bfff00d
ErinPotter <erinpotte@gmail.com>
2025-07-16 23:29:02 +0100

    testing loading data into s3 bucket

--- load_test.py
+++ load_test.py
@@ -17,4 +17,18 @@ aws_bucket_name=os.getenv('AWS_BUCKET_NAME')
 # print(aws_secret_key)
 # print(aws_bucket_name)
 
+# Create S3 Client using AWS Credentials
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=aws_access_key,
+    aws_secret_access_key=aws_secret_key
+)
 
+# Set file start and end points
+local_file_location = "data/campaigns/mailchimp_campaigns_2025-07-13_23-21-08.json"
+aws_file_destination = "python-import/campaigns/mailchimp_campaigns_2025-07-13_23-21-08.json"
+
+# Upload file to S3 Bucket
+s3_client.upload_file(local_file_location, aws_bucket_name, aws_file_destination)
+
+print("File uploaded")
